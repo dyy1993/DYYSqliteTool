ef496d88c403ffdad7ebe1839cc5f27091d6eaf9
dyy <771750834@qq.com>
2017-07-13 16:45:13 +0800

    init

---
+++ DYYSqliteTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DYYSqliteTool
+//
+//  Created by yang on 2017/7/13.
+//  Copyright © 2017年 dingyangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DYYSqliteTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DYYSqliteTool
+//
+//  Created by yang on 2017/7/13.
+//  Copyright © 2017年 dingyangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
